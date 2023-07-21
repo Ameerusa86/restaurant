9bfceb70bb1f8d65043919809b8fc6d705ce2105
Ameer Alhasnawi <46317379+Ameerusa86@users.noreply.github.com>
2023-07-20 23:50:50 -0400

    -

--- Components/CountDown.tsx
+++ Components/CountDown.tsx
@@ -1,17 +1,17 @@
-"use client";
+// "use client";
 
-import React from "react";
-import Countdown from "react-countdown";
+// import React from "react";
+// import Countdown from "react-countdown";
 
-const endingDate = new Date("2023-08-01");
+// const endingDate = new Date("2023-08-01");
 
-const CountDown = () => {
-  return (
-    <Countdown
-      className="font-bold text-5xl text-yellow-300"
-      date={endingDate}
-    />
-  );
-};
+// const CountDown = () => {
+//   return (
+//     <Countdown
+//       className="font-bold text-5xl text-yellow-300"
+//       date={endingDate}
+//     />
+//   );
+// };
 
-export default CountDown;
+// export default CountDown;
--- Components/Offer.tsx
+++ Components/Offer.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import React from "react";
-import CountDown from "./CountDown";
+// import CountDown from "./CountDown";
 
 const Offer = () => {
   return (
@@ -14,7 +14,7 @@ const Offer = () => {
           Progressively simplify effective e-toilers and process-centric methods
           of empowerment. Quickly pontificate parallel.
         </p>
-        <CountDown />
+        {/* <CountDown /> */}
         <button className="bg-red-500 text-white rounded-md py-3 px-6">
           Order Now
         </button>
