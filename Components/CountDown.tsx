aebfb466815666c4365a0c270bb79c03f3e10c30
ameerusa86 <engamermecha@gmail.com>
2023-07-21 15:14:46 -0400

    Bug fix

--- Components/CountDown.tsx
+++ Components/CountDown.tsx
@@ -2,15 +2,25 @@
 
 import React from "react";
 import Countdown from "react-countdown";
+import { useState, useEffect } from "react";
 
 const endingDate = new Date("2023-08-01");
 
 const CountDown = () => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   return (
-    <Countdown
-      className="font-bold text-5xl text-yellow-300"
-      date={endingDate}
-    />
+    <>
+      <Countdown
+        className="font-bold text-5xl text-yellow-300"
+        date={endingDate}
+      />
+      <h1>{isClient ? "This is never prerendered" : "Prerendered"}</h1>
+    </>
   );
 };
 
